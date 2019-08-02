6611875fec291a1ee681d0f4abf449b75b528e19
Han Xu <hanxu27@gmail.com>
2019-08-02 07:49:05 -0400

    redux-devtools-extension/logOnlyInProduction'

--- src/Store.js
+++ src/Store.js
@@ -1,6 +1,8 @@
 import { createStore, applyMiddleware } from "redux";
-import { composeWithDevTools } from "redux-devtools-extension";
+import { composeWithDevTools } from "redux-devtools-extension/logOnlyInProduction";
 import thunk from "redux-thunk";
 import rootReducer from "./reducers/rootReducer";
+
 const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
+
 export default store;
